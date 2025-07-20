abe9dd3f4f7d9079f0572557f7906b9ae8552095
Delaksan-Sritharan <delaksan@gmail.com>
2025-07-20 18:51:48 +0530

    feat: enhance Card component with like functionality and click counter

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 const Card = ({title}) =>{
+    const [count, setCount] = useState(0)
     const [hasLiked, setHasLiked] = useState(false);
+
+    useEffect(() => {
+        console.log(`${title} has been liked: ${hasLiked}`)
+    },[hasLiked]);
+
+
+
     return(
-        <div className="card">
-        <h2>{title}</h2>
-            <button onClick={() => setHasLiked(true)}>{hasLiked? 'Liked':'Like'}</button>
+        <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
+        <h2>{title} <br/> {count}</h2>
+            <button onClick={() => setHasLiked(!hasLiked)}>{hasLiked? '❤️️':'🤍'}</button>
         </div>
     )
 }
@@ -17,7 +25,6 @@ const App = () =>{
             <Card title="Star Wars" rating={5} isCool={true} />
             <Card title="Avatar"/>
             <Card title="The Lion King"/>
-
         </div>
     )
 }
