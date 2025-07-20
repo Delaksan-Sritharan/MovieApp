96f7e60d4f94383883982781972fb6f2953d0ba3
Delaksan-Sritharan <delaksan@gmail.com>
2025-07-20 22:01:20 +0530

    feat: add movie fetching with loading state and error handling in App component

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -20,7 +20,12 @@ const App = () => {
 
     const [errorMessage, setErrorMessage] = useState('')
 
+    const [movieList , setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
     const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
         try{
             const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
@@ -29,11 +34,17 @@ const App = () => {
                 throw new Error("Failed to fetch movies");
             }
             const data = await response.json();
-            console.log(data)
-
+            if(data.Response === "False"){
+                throw new Error(data.Error || "Failed to fetch movies");
+                setMovieList([]);
+                return;
+            }
+            setMovieList(data.results || []);
         }catch (error){
             console.error(`Error fetching movies:${error}`);
             setErrorMessage("Error fetching movies. Please try again later.");
+        }finally {
+            setIsLoading(false)
         }
     }
 
@@ -51,8 +62,17 @@ const App = () => {
                 </header>
                 <section className="all-movies">
                     <h2>All Movies</h2>
-                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
+                    {isLoading ? (
+                        <p className="text-white">Loading...</p>
+                    ):errorMessage?(
+                        <p className="text-red-500">{errorMessage}</p>
+                    ):(
+                        <ul>
+                            {movieList.map((movie) => (
+                                <p className="text-white">{movie.title}</p>
+                            ))}
+                        </ul>
+                    )}
                 </section>
             </div>
         </main>
