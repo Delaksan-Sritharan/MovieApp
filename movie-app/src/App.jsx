7432cb1eeccdcb3350ae7167006f300788f199f5
Delaksan-Sritharan <delaksan@gmail.com>
2025-07-20 19:29:30 +0530

    feat: integrate Tailwind CSS for improved styling and layout

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,31 +1,16 @@
-import './App.css'
-import {useEffect, useState} from "react";
+import React from 'react'
 
+const App = () => {
+    return (
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
 
-const Card = ({title}) =>{
-    const [count, setCount] = useState(0)
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`)
-    },[hasLiked]);
-
-
-
-    return(
-        <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
-        <h2>{title} <br/> {count || null}</h2>
-            <button onClick={() => setHasLiked(!hasLiked)}>{hasLiked? '❤️️':'🤍'}</button>
-        </div>
-    )
-}
-const App = () =>{
-    return(
-        <div className="card-container">
-            <Card title="Star Wars" rating={5} isCool={true} />
-            <Card title="Avatar"/>
-            <Card title="The Lion King"/>
-        </div>
+            </div>
+        </main>
     )
 }
 export default App
